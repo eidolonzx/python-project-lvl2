1342dedac287a52da32f31758df75d6f3d7f8355
eidolonzx <eidolonzx@gmail.com>
2022-05-14 12:38:04 +0400

    added --format cli option

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,7 +6,10 @@ def main():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    args = parser.parse_args()
+    parser.add_argument('-f', '--format', help='set format of output', default='plain', choices=['plain', 'text', 'json'])
+    # args = parser.parse_args()
+    parser.parse_args()
+
 
 if __name__ == '__main__':
     main()
