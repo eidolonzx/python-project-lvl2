6c47e7dc4fe348f1ea60b11b5da2618ed9808366
eidolonzx <eidolonzx@gmail.com>
2022-05-24 14:37:50 +0400

    feat: add json formatter

---
+++ tests/test_complex_json.py
@@ -0,0 +1,13 @@
+from gendiff.engine import generate_diff
+from gendiff.render import render_diff_result
+
+def test_plain_render():
+    filepath1 = 'tests/fixtures/file1_complex.json'
+    filepath2 = 'tests/fixtures/file2_complex.json'
+    result_file = open('tests/fixtures/result_json_complex.txt', 'r')
+    result_output = result_file.read()
+
+    diff_result = generate_diff(filepath1, filepath2)
+    
+    assert render_diff_result(diff_result, 'json') == result_output
+    
